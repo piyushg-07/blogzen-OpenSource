3aed70a65691ff6750ff8a0817aa769078ad0c48
Subhams-GIT <subhamkumardas4444@gmail.com>
2024-07-30 11:47:22 +0530

    fixed like button issue

--- js/blog.js
+++ js/blog.js
@@ -11,10 +11,13 @@ document.addEventListener("DOMContentLoaded", function() {
       .then(res => res.json())
       .then(json => {
         json.blogs.map(data => {
-          console.log("data1", data);
+          //console.log("data1", data);
           // tbody.innerHTML += td_fun(data);
           // tbody.append(td_blog(data));
-         if (data.id === id) tbody.innerHTML = td_blog(data).innerHTML;
+         if (data.id === id) {
+          tbody.innerHTML = td_blog(data).innerHTML;
+          initliase(data.id);
+         }
         });
       })
       .catch(error => {
@@ -22,6 +25,22 @@ document.addEventListener("DOMContentLoaded", function() {
       });
   });
 
+  function initliase(id){
+    const likecount=localStorage.getItem(`likecount-${id}`) || 0; //get the like count if its nan then 0 will be displayed 
+    console.log(like);
+    document.getElementById(`likecount-${id}`).innerHTML=likecount;
+  }
+
+  function setlike(item){
+    console.log(item);
+    const like=document.getElementById(`likecount-${'00'+item}`)
+    console.log(like);
+    let currentlike=parseInt(like.innerHTML,10);
+   like.innerHTML=currentlike+1;
+   localStorage.setItem(`likecount-${'00'+item}`,parseInt(like.innerHTML)); //set the like countfor each blog
+
+  }
+ 
   // create td
   function td_blog(item) {
     console.log("item000", item ,item.id);
@@ -32,7 +51,7 @@ document.addEventListener("DOMContentLoaded", function() {
         <div class="title_container">
         <div class="category">${item.tags}</div>
         <div class="title">${item.title}</div>
-        <div class="info">A conversation with Fereshteh Forough, founder and president of Afghanistan’s Code to Inspire
+        <div class="info">A conversation with Fereshteh Forough, founder and president of Afghanistan's Code to Inspire
         </div>
         <div class="author_details">
             <div class="profile_image"><img src="/assests/profile_image_large.png" alt=""></div>
@@ -56,8 +75,8 @@ document.addEventListener("DOMContentLoaded", function() {
         <div class="like_comment_section">
             <hr>
             <div class="buttons_l_c">
-                <img class="like_icon" src="assests/like_icon.png" alt="">
-                <span>342</span>
+               <button id="like" onclick="setlike(${item.id})" ><img class="like_icon" src="assests/like_icon.png" alt=""></button> 
+                <span id="likecount-${item.id}" >${item.like}</span>
                 <img class="comment_icon" src="assests/comment_icon.png" alt="">
             </div>
             <hr class="below">
